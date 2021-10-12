a7b2d15d3ce3fa411020059af36eac22c9293892
Udy Ding <udy.ding@gmail.com>
2021-10-12 18:46:23 -0400

    redesign + simplify

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import styles from "./App.module.css";
-import Greeting from "./Greeting";
-import AboutMe from "./AboutMe";
-import Projects from "./Projects";
-import Resume from "./Resume";
-import Contact from "./Contact";
-import Footer from "./Footer";
+// import Greeting from "./Greeting";
+// import AboutMe from "./AboutMe";
+// import Projects from "./Projects";
+// import Resume from "./Resume";
+// import Contact from "./Contact";
+// import Footer from "./Footer";
+import Simple from "./Simple";
 
 function App() {
   return (
     <div className={styles.container}>
-      <Greeting />
+      <Simple />
+      {/* <Greeting />
       <AboutMe />
       <Projects />
       <Resume />
       <a id="contact" />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Simple.js
@@ -0,0 +1,100 @@
+import React from "react";
+import styles from "./Simple.module.css";
+import portrait from "../assets/Portrait.jpg";
+import emailIcon from "../assets/contact/Email.png";
+import githubIcon from "../assets/contact/Github.png";
+import linkedinIcon from "../assets/contact/LinkedIn.png";
+
+export default function Simple() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img className={styles.portrait} src={portrait} alt="portrait" />
+        <div className={styles.contact}>
+          <div className={styles.media}>
+            <a
+              href="mailto:udy.ding@gmail.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={emailIcon} alt="email" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/udy-ding/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={linkedinIcon} alt="linkedin" />
+            </a>
+            <a
+              href="http://github.com/udyding"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={githubIcon} alt="github" />
+            </a>
+          </div>
+          <div className={styles.resume}>
+            <a
+              href="Udy Ding Resume Fall 2021.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button>Resume</button>
+            </a>
+          </div>
+
+          <div className={styles.resume}></div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title}>Hi there! I'm Udy.</div>
+        <div className={styles.description}>
+          I’m a computer science student at UWaterloo and an incoming SWE intern
+          at{" "}
+          <a
+            href="https://www.wish.com/companyinfo?hide_login_modal=true"
+            rel="noopener noreferrer"
+            target="_blank"
+            style={{
+              fontWeight: "800",
+              color: "#ffffff",
+              textDecoration: "none",
+            }}
+          >
+            Wish
+          </a>
+          . Previously, I interned at OpenText working on their{" "}
+          <a
+            href="https://www.opentext.com/products-and-solutions/products/opentext-core-share"
+            rel="noopener noreferrer"
+            target="_blank"
+            style={{
+              fontWeight: "800",
+              color: "#ffffff",
+              textDecoration: "none",
+            }}
+          >
+            Core Share
+          </a>{" "}
+          software.
+          <br />
+          <br />
+          Programming is my outlet for creating interesting solutions to
+          everyday problems. What I could once only accomplish by reaching out
+          locally, I can now translate to apps anyone can access.
+          <br />
+          <br />
+          Some things I am currently working on are: mastering dorm cooking,
+          learning new tech, and getting a pet hedgehog.
+          <br />
+          <br />
+          <span style={{ fontWeight: "800" }}>
+            I am currently looking for Summer and Fall 2022 opportunities!
+          </span>{" "}
+          If you’d like to chat, please feel free to reach out.
+        </div>
+      </div>
+    </div>
+  );
+}
